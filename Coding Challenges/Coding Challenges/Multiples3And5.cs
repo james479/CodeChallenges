1e3febf8f86f58358c470f59ec043dac89641297
james479 <james.e.glover57@gmail.com>
2021-03-09 05:33:16 -0600

    iniital commit

---
+++ Coding Challenges/Coding Challenges/Multiples3And5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Challenges
+{
+    public class Multiples3And5
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+
+            for(int i = 1; i < 1000; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    total += i;
+                }
+            }
+
+            Console.WriteLine($"Total sum is: {total}");
+        }
+    }
+}
